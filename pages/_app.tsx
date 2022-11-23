01267707cb7f83eaca25ff872bb22c2f9c7767ea
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-11-23 20:39:05 +0100

    Working communication with supabase backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import Fonts from '../components/fonts';
@@ -6,23 +7,32 @@ import Layout from '../components/layouts/layout';
 import theme from '../libs/theme';
 import '../styles/globals.css';
 
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+
 export default function App({ Component, pageProps, router }: AppProps) {
+	const [supabase] = useState(() => createBrowserSupabaseClient());
 	return (
-		<ChakraProvider theme={theme}>
-			<Fonts />
-			<Layout>
-				<AnimatePresence
-					exitBeforeEnter
-					initial={true}
-					onExitComplete={() => {
-						if (typeof window !== 'undefined') {
-							window.scrollTo({ top: 0 });
-						}
-					}}
-				>
-					<Component {...pageProps} key={router.route} />
-				</AnimatePresence>
-			</Layout>
-		</ChakraProvider>
+		<SessionContextProvider
+			supabaseClient={supabase}
+			initialSession={pageProps.initialSession}
+		>
+			<ChakraProvider theme={theme}>
+				<Fonts />
+				<Layout>
+					<AnimatePresence
+						exitBeforeEnter
+						initial={true}
+						onExitComplete={() => {
+							if (typeof window !== 'undefined') {
+								window.scrollTo({ top: 0 });
+							}
+						}}
+					>
+						<Component {...pageProps} key={router.route} />
+					</AnimatePresence>
+				</Layout>
+			</ChakraProvider>
+		</SessionContextProvider>
 	);
 }
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -13,64 +13,104 @@ import {
 	useColorModeValue,
 } from '@chakra-ui/react';
 import Content from '../components/layouts/content';
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
+
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import  Account  from './account';
+import { useEffect, useState } from 'react';
+import { supabase } from '../libs/supabaseClient';
+import { AuthSession } from '@supabase/supabase-js';
 
 export default function SimpleCard() {
-	return (
-		<Flex minH={'93vh'} align={'center'} justify={'center'}>
-			<Content>
-				<Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-					<Stack align={'center'}>
-						<Heading fontSize={'4xl'}>
-							Sign in to your account
-						</Heading>
-						<Text fontSize={'lg'} color={'gray.600'}>
-							to enjoy all of our cool{' '}
-							<Link color={'blue.400'}>features</Link> ✌️
-						</Text>
-					</Stack>
-					<Box
-						rounded={'lg'}
-						bg={useColorModeValue('white', 'gray.700')}
-						boxShadow={'lg'}
-						p={8}
-					>
-						<Stack spacing={4}>
-							<FormControl id="email">
-								<FormLabel>Email address</FormLabel>
-								<Input type="email" />
-							</FormControl>
-							<FormControl id="password">
-								<FormLabel>Password</FormLabel>
-								<Input type="password" />
-							</FormControl>
-							<Stack spacing={10}>
-								<Stack
-									direction={{ base: 'column', sm: 'row' }}
-									align={'start'}
-									justify={'space-between'}
-								>
-									<Checkbox>Remember me</Checkbox>
-									<Link
-										color={'blue.400'}
-										href="/forgot-password"
-									>
-										Forgot password?
-									</Link>
-								</Stack>
-								<Button
-									bg={'blue.400'}
-									color={'white'}
-									_hover={{
-										bg: 'blue.500',
-									}}
-								>
-									Sign in
-								</Button>
-							</Stack>
-						</Stack>
-					</Box>
-				</Stack>
-			</Content>
-		</Flex>
-	);
+	  const [session, setSession] = useState<AuthSession | null>(null);
+
+		useEffect(() => {
+			async function fetchData() {
+				// You can await here
+				const session = (await supabase.auth.getSession()).data.session;
+				setSession(session);
+				// ...
+			}
+			fetchData();
+
+			supabase.auth.onAuthStateChange(
+				(_event: string, session: AuthSession | null) => {
+					setSession(session);
+				}
+			);
+		}, []);
+
+
+	  return (
+			<div className="container" style={{ padding: '50px 0 100px 0' }}>
+				{!session ? (
+					<Auth
+						supabaseClient={supabase}
+						appearance={{ theme: ThemeSupa }}
+						theme="dark"
+					/>
+				) : (
+					<Account key={session.user.id} session={session} />
+				)}
+			</div>
+		);
+	
+	// return (
+	// 	<Flex minH={'93vh'} align={'center'} justify={'center'}>
+	// 		<Content>
+	// 			<Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+	// 				<Stack align={'center'}>
+	// 					<Heading fontSize={'4xl'}>
+	// 						Sign in to your account
+	// 					</Heading>
+	// 					<Text fontSize={'lg'} color={'gray.600'}>
+	// 						to enjoy all of our cool{' '}
+	// 						<Link color={'blue.400'}>features</Link> ✌️
+	// 					</Text>
+	// 				</Stack>
+	// 				<Box
+	// 					rounded={'lg'}
+	// 					bg={useColorModeValue('white', 'gray.700')}
+	// 					boxShadow={'lg'}
+	// 					p={8}
+	// 				>
+	// 					<Stack spacing={4}>
+	// 						<FormControl id="email">
+	// 							<FormLabel>Email address</FormLabel>
+	// 							<Input type="email" />
+	// 						</FormControl>
+	// 						<FormControl id="password">
+	// 							<FormLabel>Password</FormLabel>
+	// 							<Input type="password" />
+	// 						</FormControl>
+	// 						<Stack spacing={10}>
+	// 							<Stack
+	// 								direction={{ base: 'column', sm: 'row' }}
+	// 								align={'start'}
+	// 								justify={'space-between'}
+	// 							>
+	// 								<Checkbox>Remember me</Checkbox>
+	// 								<Link
+	// 									color={'blue.400'}
+	// 									href="/forgot-password"
+	// 								>
+	// 									Forgot password?
+	// 								</Link>
+	// 							</Stack>
+	// 							<Button
+	// 								bg={'blue.400'}
+	// 								color={'white'}
+	// 								_hover={{
+	// 									bg: 'blue.500',
+	// 								}}
+	// 							>
+	// 								Sign in
+	// 							</Button>
+	// 						</Stack>
+	// 					</Stack>
+	// 				</Box>
+	// 			</Stack>
+	// 		</Content>
+	// 	</Flex>
+	// );
 }
