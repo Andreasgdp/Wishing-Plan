8b74f72728b6f8717b916d6518e2844e09bb8272
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-12-10 21:56:11 +0100

    Remove supabase stuff

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,7 +1,3 @@
-export const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const NEXT_PUBLIC_SUPABASE_ANON_KEY =
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
 export const DEFAULT_AVATARS_BUCKET = 'avatars';
 
 export type Profile = {
